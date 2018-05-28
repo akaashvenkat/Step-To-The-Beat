c3ee520310cbd9d02498181a2739ae53c8d9a803
akaashvenkat <akaashvenkat@s-169-232-116-22.resnet.ucla.edu>
2018-05-27 18:45:36 -0700

    added bluetooth configuration

--- comsci117/app/src/main/java/com/example/android/comsci117/MainActivity.java
+++ comsci117/app/src/main/java/com/example/android/comsci117/MainActivity.java
@@ -6,6 +6,8 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,112 +27,225 @@ import com.wahoofitness.connector.listeners.discovery.DiscoveryListener;
 
 import org.w3c.dom.Text;
 
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.res.Configuration;
-import android.hardware.Camera;
-import android.hardware.Camera.PreviewCallback;
-import android.os.Bundle;
-import android.os.PowerManager;
-import android.os.PowerManager.WakeLock;
-import android.util.Log;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.view.View;
-import android.widget.TextView;
-
+import static java.lang.Math.min;
 
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
     private static final String TAG = "MyActivity";
-    boolean active = false;
-    SensorManager sensorManager;
+    String month, day;
+    public TextView heartbeat;
     TextView steps_titleTV;
     TextView stepsTV;
     TextView heartbeat_title;
     TextView heartbeat;
-    String month, day;
     Button resetButton;
+    Button start_hr;
     int total_steps;
     int recent_steps;
+    int current_steps = 15;
+    int i = 0;
+    boolean active = false;
+
+    SensorManager sensorManager;
+    DiscoveryListener discover_listener;
+    HeartbeatServices mHeartbeatServices;
+    ConnectionParams mConnectionParams;
+    Heartrate hr;
+    Heartrate.Listener mHeartrateListener;
+    SensorConnection.Listener heartrate_listener;
+
+    private HardwareConnector mHardwareConnector;
+    private SensorConnection mSensorConnection;
+    private final HardwareConnector.Callback mHardwareConnectorCallback = new HardwareConnector.Callback() {
+        @Override
+        public void disconnectedSensor(SensorConnection sensorConnection) { return; }
+
+        @Override
+        public void connectorStateChanged(HardwareConnectorTypes.NetworkType networkType, HardwareConnectorEnums.HardwareConnectorState hardwareConnectorState) { return; }
+
+        @Override
+        public void connectedSensor(SensorConnection sensorConnection) { return; }
+
+        @Override
+        public void onFirmwareUpdateRequired(SensorConnection sensorConnection, String s, String s1) {}
+
+        @Override
+        public void hasData() {}
+    };
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        heartbeat_title = (TextView) findViewById(R.id.heartbeat_title);
         heartbeat = (TextView) findViewById(R.id.heartbeat);
+        heartbeat_title = (TextView) findViewById(R.id.heartbeat_title);
         steps_titleTV = (TextView) findViewById(R.id.steps_title);
         stepsTV = (TextView) findViewById(R.id.steps);
         resetButton = (Button) findViewById(R.id.reset_button);
+        start_hr = (Button) findViewById(R.id.start_hr);
 
         SharedPreferences saved_steps = getSharedPreferences("saved_steps", Context.MODE_PRIVATE);
         recent_steps = saved_steps.getInt("recentSteps", -1);
 
-        resetButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+        resetButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
 
                 SharedPreferences saved_steps = getSharedPreferences("saved_steps", Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = saved_steps.edit();
                 editor.putInt("recentSteps", total_steps);
                 editor.commit();
+                current_steps = total_steps - saved_steps.getInt("recentSteps", -1);
 
-                stepsTV.setText(String.valueOf(total_steps - saved_steps.getInt("recentSteps", -1)));
+                stepsTV.setText(String.valueOf(current_steps));
+            }
+        });
 
-                //Toast.makeText(MainActivity.this, String.valueOf(recent_steps), Toast.LENGTH_LONG).show();
+        start_hr.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                if (heartbeat.getText().equals("stop"))
+                {
+                    timerHandler.removeCallbacks(timerRunnable);
+                }
+                else
+                {
+                    i = 0;
+                    startTime = System.currentTimeMillis();
+                    timerHandler.postDelayed(timerRunnable, 2000);
+                }
             }
         });
 
 
+        discover_listener = new DiscoveryListener()
+        {
+            public void onDeviceDiscovered(ConnectionParams connectionParams)
+            {
+                Toast.makeText(MainActivity.this, "Discovered heartrate device.", Toast.LENGTH_LONG).show();
+                Log.d(TAG, "Found heartrate device");
+                heartrate_listener = new SensorConnection.Listener()
+                {
+                    @Override
+                    public void onSensorConnectionStateChanged(SensorConnection sensorConnection, HardwareConnectorEnums.SensorConnectionState sensorConnectionState) {}
+
+                    @Override
+                    public void onSensorConnectionError(SensorConnection sensorConnection, HardwareConnectorEnums.SensorConnectionError sensorConnectionError) {}
+
+                    @Override
+                    public void onNewCapabilityDetected(SensorConnection sensorConnection, Capability.CapabilityType capabilityType)
+                    {
+                        if (capabilityType == Capability.CapabilityType.Heartrate)
+                        {
+                            hr = (Heartrate) sensorConnection.getCurrentCapability(Capability.CapabilityType.Heartrate);
+                            hr.addListener(mHeartrateListener);
+                        }
+                    }
+                };
+            }
+
+            public void onDiscoveredDeviceLost(ConnectionParams connectionParams)
+            {
+                Toast.makeText(MainActivity.this, "Lost connections to heartrate device.", Toast.LENGTH_LONG).show();
+            }
+
+            public void onDiscoveredDeviceRssiChanged(ConnectionParams connectionParams, int i) {}
+        };
 
 
+        mHardwareConnector = new HardwareConnector(this, mHardwareConnectorCallback);
+        mHardwareConnector.startDiscovery(HardwareConnectorTypes.SensorType.HEARTRATE, HardwareConnectorTypes.NetworkType.BTLE, discover_listener);
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        total_steps = (int) event.values[0];
+    Heartrate.Data getHeartRateData()
+    {
+      if (mSensorConnection != null && hr != null)
+      {
+          heartbeat.setText(hr.getHeartrateData().getHeartrate().toString());
+          return hr.getHeartrateData();
+      }
+      else
+        return null;
+    }
 
-        if (active == true) {
+    long startTime = 0;
+
+    Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable()
+    {
+        @Override
+        public void run()
+        {
+            Log.v("Nothing", String.valueOf(current_steps));
+
+            if (i < 11)
+            {
+                heartbeat.setText(String.format("%d", getHeartRateData()));
+                i++;
+            }
+            else
+            {
+                heartbeat.setText("--");
+            }
+            timerHandler.postDelayed(this, 2000);
+        }
+    };
+
+    public int aa(int it)
+    {
+        return it + 1;
+    }
+    public void startHRfunction(View view){}
+
+    @Override
+    public void onSensorChanged(SensorEvent event)
+    {
+        total_steps = (int) event.values[0];
+        if (active == true)
+        {
             SharedPreferences saved_steps = getSharedPreferences("saved_steps", Context.MODE_PRIVATE);
             recent_steps = saved_steps.getInt("recentSteps", -1);
-            stepsTV.setText(String.valueOf(total_steps - recent_steps));
+            current_steps = total_steps - recent_steps;
+            stepsTV.setText(String.valueOf(current_steps));
         }
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
 
     @Override
-    protected void onResume() {
+    protected void onResume()
+    {
         super.onResume();
         active = true;
         Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
 
-        if (countSensor != null) {
+        if (countSensor != null)
+        {
             sensorManager.registerListener(this, countSensor, sensorManager.SENSOR_DELAY_UI);
-        } else {
+        }
+        else
+        {
             Toast.makeText(this, "Cannot find sensor", Toast.LENGTH_LONG).show();
         }
-        //Heartrate.Data data = mHeartbeatServices.getHeartRateData();
-        //heartbeat.setText(data.toString());
-
-
-
     }
 
-
     @Override
-    protected void onPause() {
+    protected void onPause()
+    {
         super.onPause();
         active = false;
     }
-
-
 }
