9667519fb1bdb558b83759c618eaa95ebaa296c1
akaashvenkat <akaashvenkat@gmail.com>
2018-05-27 20:06:36 -0700

    Revert "final cleanup"
    This reverts commit 0bbaeaf1f8d20a2b4d296e634bf5bfbc9a39fc6e.

--- app/libs/sdk/com.wahoofitness.api.docset/Contents/Resources/Documents/protocol_w_f_ant_f_s_device_delegate-p.js
+++
@@ -1,4 +0,0 @@
-var protocol_w_f_ant_f_s_device_delegate_p =
-[
-    [ "antFSDevice:instanceCreated:", "protocol_w_f_ant_f_s_device_delegate-p.html#a3b82674722ca6750d838f1000da91d22", null ]
-];
--- app/libs/sdk/com.wahoofitness.api.docset/Contents/Resources/Documents/search/typedefs_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['double',['DOUBLE',['../types_8h.html#aeb90fd78c942591ef3a935cf98baf4f8',1,'types.h']]]
-];
